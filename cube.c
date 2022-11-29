1e8b0b4db5f7cf688045aa64f398b77378e381c3
UsadJaydip <114413179+UsadJaydip@users.noreply.github.com>
2022-11-29 17:06:01 +0530

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int num1,ans;
+	
+	printf("\n enter number of value :");
+	scanf("%d",&num1);
+	
+	ans=num1*num1;
+	
+	printf("ans=%d",ans);
+    
+	ans=num1*num1*num1;
+	
+	printf("\n ans=%d",ans);	
+	
+}
